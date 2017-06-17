fee50637389ef76f7a12b7c1ed068381bf20aebb
Oleksandr Rozdolskyi <orozdolskyi@gmail.com>
2017-06-17 17:26:34 +0300

    cover email receiving service  with unit test

--- src/main/java/edu/hneu/studentsportal/service/StudentEmailReceivingService.java
+++ src/main/java/edu/hneu/studentsportal/service/StudentEmailReceivingService.java
@@ -5,7 +5,7 @@ import javaslang.control.Try;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestOperations;
 
 import javax.annotation.Resource;
 
@@ -16,6 +16,8 @@ public class StudentEmailReceivingService {
 
     @Resource
     private MessageService messageService;
+    @Resource
+    private RestOperations restTemplate;
     @Value("${integration.service.emails.url}")
     public String emailsIntegrationServiceUrl;
 
@@ -24,7 +26,7 @@ public class StudentEmailReceivingService {
         String formatterSurname = student.getSurname().toLowerCase().trim();
         String groupName = student.getGroup().getName();
         String url = format("%s/EmailToOutController?name=%s&surname=%s&groupId=%s", emailsIntegrationServiceUrl, formattedName, formatterSurname, groupName);
-        return Try.of(() -> new RestTemplate().getForEntity(url, String.class))
+        return Try.of(() -> restTemplate.getForEntity(url, String.class))
                 .map(ResponseEntity::getBody)
                 .map(String::toLowerCase)
                 .getOrElseThrow(() -> new IllegalArgumentException(messageService.emailNotFoundForStudent(formattedName + " " + formatterSurname)));
---
+++ src/test/groovy/edu/hneu/studentsportal/service/StudentEmailReceivingServiceTest.groovy
@@ -0,0 +1,55 @@
+package edu.hneu.studentsportal.service
+
+import edu.hneu.studentsportal.domain.Group
+import edu.hneu.studentsportal.domain.Student
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestTemplate
+import spock.lang.Specification
+
+class StudentEmailReceivingServiceTest extends Specification {
+
+    static final String EMAIL_INTEGRATION_SERVICE_URL = 'test.email.service'
+    static final String STUDENT_EMAIL = 'student@email'
+    static final String REQUEST_EMAIL_URL = 'test.email.service/EmailToOutController?name=олександр&surname=роздольський&groupId=8.04.51.16.01'
+    public static final String ERROR_MESSAGE = 'error message'
+
+    def messageServiceMock = Mock(MessageService)
+    def studentMock = Mock(Student)
+    def groupMock = Mock(Group)
+    def restTemplateMock = Mock(RestTemplate)
+    def restEntityMock = Mock(ResponseEntity)
+
+    def studentEmailReceivingService = new StudentEmailReceivingService(
+        messageService: messageServiceMock,
+        emailsIntegrationServiceUrl: EMAIL_INTEGRATION_SERVICE_URL,
+        restTemplate: restTemplateMock
+    )
+
+    def setup() {
+        studentMock.getName() >> 'Олександр'
+        studentMock.getSurname() >> 'Роздольський'
+        studentMock.getGroup() >> groupMock
+        groupMock.getName() >> '8.04.51.16.01'
+        restTemplateMock.getForEntity(REQUEST_EMAIL_URL, String.class) >> restEntityMock
+    }
+
+    def 'should return student email when service responded correctly'() {
+        given:
+            restEntityMock.getBody() >> STUDENT_EMAIL
+        when:
+            def email = studentEmailReceivingService.receiveStudentEmail(studentMock)
+        then:
+            STUDENT_EMAIL == email
+    }
+
+    def 'should throw exception when student email was not found'() {
+        given:
+            restEntityMock.getBody() >> null
+            messageServiceMock.emailNotFoundForStudent(*_) >> ERROR_MESSAGE
+        when:
+            studentEmailReceivingService.receiveStudentEmail(studentMock)
+        then:
+            IllegalArgumentException ex = thrown()
+            ex.message == ERROR_MESSAGE
+    }
+}
